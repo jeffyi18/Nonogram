b1bc3729ecdd1203b55a68e80fc400b21a62ab43
jeffyi18 <jeff123@live.unc.edu>
2020-11-11 17:46:56 -0500

    first

--- src/main/java/com/comp301/a08nonograms/controller/ControllerImpl.java
+++ src/main/java/com/comp301/a08nonograms/controller/ControllerImpl.java
@@ -1,5 +1,6 @@
 package com.comp301.a08nonograms.controller;
 
+import java.util.Random;
 import com.comp301.a08nonograms.model.Clues;
 import com.comp301.a08nonograms.model.Model;
 
@@ -18,56 +19,69 @@ public class ControllerImpl implements Controller {
 
   @Override
   public boolean isSolved() {
-    return false;
+    return currentModel.isSolved();
   }
 
   @Override
   public boolean isShaded(int row, int col) {
-    return false;
+    return currentModel.isShaded(row, col);
   }
 
   @Override
   public boolean isEliminated(int row, int col) {
-    return false;
+    return currentModel.isEliminated(row, col);
   }
 
   @Override
   public void toggleShaded(int row, int col) {
-
+    currentModel.toggleCellShaded(row, col);
   }
 
   @Override
   public void toggleEliminated(int row, int col) {
-
+    currentModel.toggleCellEliminated(row, col);
   }
 
   @Override
   public void nextPuzzle() {
-
+    int index = currentModel.getPuzzleIndex();
+    if (index == 4) {
+      currentModel.setPuzzleIndex(0);
+    } else {
+      currentModel.setPuzzleIndex(index + 1);
+    }
   }
 
   @Override
   public void prevPuzzle() {
-
+    int index = currentModel.getPuzzleIndex();
+    if (index == 0) {
+      currentModel.setPuzzleIndex(4);
+    } else {
+      currentModel.setPuzzleIndex(index - 1);
+    }
   }
 
   @Override
   public void randPuzzle() {
-
+    Random rand = new Random();
+    int upperbound = 5;
+    int randomIndex = rand.nextInt(upperbound);
+    currentModel.setPuzzleIndex(randomIndex);
   }
 
   @Override
   public void clearBoard() {
-
+    currentModel.clear();
   }
 
   @Override
   public int getPuzzleIndex() {
-    return 0;
+    return currentModel.getPuzzleIndex();
   }
 
   @Override
   public int getPuzzleCount() {
-    return 0;
+    return currentModel.getPuzzleCount();
   }
 }
