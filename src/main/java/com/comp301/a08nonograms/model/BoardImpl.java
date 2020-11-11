6e7d291949feb82db919fc5fb691ea796aa2d5d3
jeffyi18 <jeff123@live.unc.edu>
2020-11-10 22:32:34 -0500

    first

--- src/main/java/com/comp301/a08nonograms/model/BoardImpl.java
+++ src/main/java/com/comp301/a08nonograms/model/BoardImpl.java
@@ -2,33 +2,55 @@ package com.comp301.a08nonograms.model;
 
 public final class BoardImpl implements Board {
 
+  private String[][] boardState;
+
+  public BoardImpl (int width, int height) {
+    for (int i = 0; i < width; i++) {
+      for (int j = 0; j < height; j++) {
+        boardState[i][j] = "blank";
+      }
+    }
+  }
+
   @Override
   public boolean isShaded(int row, int col) {
-    return false;
+    return boardState[row][col].equals("shaded");
   }
 
   @Override
   public boolean isEliminated(int row, int col) {
-    return false;
+    return boardState[row][col].equals("eliminated");
   }
 
   @Override
   public boolean isSpace(int row, int col) {
-    return false;
+    return boardState[row][col].equals("blank");
   }
 
   @Override
   public void toggleCellShaded(int row, int col) {
-
+    if (boardState[row][col].equals("shaded")) {
+      boardState[row][col] = "blank";
+    } else {
+      boardState[row][col] = "shaded";
+    }
   }
 
   @Override
   public void toggleCellEliminated(int row, int col) {
-
+    if (boardState[row][col].equals("eliminated")) {
+      boardState[row][col] = "blank";
+    } else {
+      boardState[row][col] = "eliminated";
+    }
   }
 
   @Override
   public void clear() {
-
+    for (int i = 0; i < boardState.length; i++) {
+      for (int j = 0; j < boardState[0].length; j++) {
+        boardState[i][j] = "blank";
+      }
+    }
   }
 }
--- src/main/java/com/comp301/a08nonograms/model/ModelImpl.java
+++ src/main/java/com/comp301/a08nonograms/model/ModelImpl.java
@@ -4,36 +4,52 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ModelImpl implements Model {
-
+  private List<ModelObserver> observers;
   private ArrayList<Clues> clues;
+  private Clues currentClues;
+  private int puzzleIndex;
+  private Board puzzleBoard;
 
   public ModelImpl(ArrayList<Clues> clues) {
+    observers = new ArrayList<>();
     this.clues = clues;
+    puzzleIndex = 0;
+    puzzleBoard = new BoardImpl(0,0);
+    setPuzzleIndex(0);
   }
 
   @Override
   public int getPuzzleCount() {
-    return 0;
+    return clues.size();
   }
 
   @Override
   public int getPuzzleIndex() {
-    return 0;
+    return puzzleIndex;
   }
 
   @Override
   public void setPuzzleIndex(int index) {
+    puzzleIndex = index;
+    puzzleBoard = new BoardImpl(clues.get(index).getWidth(), clues.get(index).getHeight());
+    currentClues = clues.get(index);
+    notifyObservers();
+  }
 
+  public void notifyObservers() {
+    for (ModelObserver i: observers) {
+      i.update(this);
+    }
   }
 
   @Override
   public void addObserver(ModelObserver observer) {
-
+    observers.add(observer);
   }
 
   @Override
   public void removeObserver(ModelObserver observer) {
-
+    observers.remove(observer);
   }
 
   @Override
@@ -43,61 +59,60 @@ public class ModelImpl implements Model {
 
   @Override
   public boolean isShaded(int row, int col) {
-    return false;
+    return puzzleBoard.isShaded(row,col);
   }
 
   @Override
   public boolean isEliminated(int row, int col) {
-    return false;
+    return puzzleBoard.isEliminated(row,col);
   }
 
   @Override
   public boolean isSpace(int row, int col) {
-    return false;
+    return puzzleBoard.isSpace(row,col);
   }
 
   @Override
   public void toggleCellShaded(int row, int col) {
-
+    puzzleBoard.toggleCellShaded(row,col);
   }
 
   @Override
   public void toggleCellEliminated(int row, int col) {
-
+    puzzleBoard.toggleCellEliminated(row,col);
   }
 
   @Override
   public void clear() {
-
+    puzzleBoard.clear();
   }
 
   @Override
   public int getWidth() {
-    return 0;
+    return currentClues.getWidth();
   }
 
   @Override
   public int getHeight() {
-    return 0;
+    return currentClues.getHeight();
   }
 
   @Override
   public int[] getRowClues(int index) {
-    return new int[0];
+  return currentClues.getRowClues(index);
   }
 
   @Override
   public int[] getColClues(int index) {
-    return new int[0];
+    return currentClues.getColClues(index);
   }
 
   @Override
-  public int getRowCluesLength() {
-    return 0;
+  public int getRowCluesLength() { return currentClues.getRowCluesLength();
   }
 
   @Override
   public int getColCluesLength() {
-    return 0;
+    return currentClues.getColCluesLength();
   }
 }
