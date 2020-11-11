d83c784d854f5c1914979ab21e1512683fa7c8c7
jeffyi18 <jeff123@live.unc.edu>
2020-11-10 23:29:51 -0500

    first

--- src/main/java/com/comp301/a08nonograms/model/BoardImpl.java
+++ src/main/java/com/comp301/a08nonograms/model/BoardImpl.java
@@ -3,8 +3,12 @@ package com.comp301.a08nonograms.model;
 public final class BoardImpl implements Board {
 
   private String[][] boardState;
+  private int width;
+  private int height;
 
   public BoardImpl (int width, int height) {
+    this.width = width;
+    this.height = height;
     boardState = new String[width][height];
     for (int i = 0; i < width; i++) {
       for (int j = 0; j < height; j++) {
@@ -15,21 +19,33 @@ public final class BoardImpl implements Board {
 
   @Override
   public boolean isShaded(int row, int col) {
+    if (row >= width || col >= height) {
+      throw new RuntimeException();
+    }
     return boardState[row][col].equals("shaded");
   }
 
   @Override
   public boolean isEliminated(int row, int col) {
+    if (row >= width || col >= height) {
+      throw new RuntimeException();
+    }
     return boardState[row][col].equals("eliminated");
   }
 
   @Override
   public boolean isSpace(int row, int col) {
+    if (row >= width || col >= height) {
+      throw new RuntimeException();
+    }
     return boardState[row][col].equals("blank");
   }
 
   @Override
   public void toggleCellShaded(int row, int col) {
+    if (row >= width || col >= height) {
+      throw new RuntimeException();
+    }
     if (boardState[row][col].equals("shaded")) {
       boardState[row][col] = "blank";
     } else {
@@ -39,6 +55,9 @@ public final class BoardImpl implements Board {
 
   @Override
   public void toggleCellEliminated(int row, int col) {
+    if (row >= width || col >= height) {
+      throw new RuntimeException();
+    }
     if (boardState[row][col].equals("eliminated")) {
       boardState[row][col] = "blank";
     } else {
